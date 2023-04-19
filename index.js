d7d20954e64fdb39fd1169b4195077e09ccf2fd5
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-04-18 22:16:12 -0400

    complete fetch lab

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  //shorter way to return response and then send it to my renderBoks or passing to the DOM
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(data => renderBooks(data))
+  
+  
   
 }
 
